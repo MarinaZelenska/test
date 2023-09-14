8b193cd53279223b759e22b0505a6b80a25ae622
maryna.zelenska <maryna.zelenska.-nd@disney.com>
2023-09-14 21:01:45 +0300

    initial commit

---
+++ Lesson_2/lesson_2.py
@@ -0,0 +1,31 @@
+
+# integer -> int
+# float
+# complex
+
+# import math
+# from math import sqrt
+
+# print(math.pi)
+# print(math.e)
+# print(math.sqrt(144))
+# print(math.floor(1.6))
+# print(int(1.6))
+# print(sqrt(144))
+
+# import random
+
+# print(random.random())
+# print(random.randint(1, 10))
+# print(random.choice([1, 2, 3, 4, 5, 6]))
+# lst = [1, 2, 3, 4, 5, 6]
+# random.shuffle(lst)
+# print(lst)
+
+
+# print(0.1 + 0.1 + 0.1 - 0.3)
+#
+# from decimal import Decimal
+#
+# test = Decimal(0.1) + Decimal(0.1) + Decimal(0.1) - Decimal(0.3)
+# print(test)
