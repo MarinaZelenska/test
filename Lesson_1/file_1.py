9a94648ab0d4b598c66d6b08dbe8fed871aeff45
maryna.zelenska <maryna.zelenska.-nd@disney.com>
2023-09-14 14:05:17 +0300

    first commit

---
+++ Lesson_1/file_1.py
@@ -0,0 +1,111 @@
+# x, y, z = 10, 19, "Hillel"
+# print(x)
+# print(y)
+# print(z)
+# x = y = z = "LMS"
+# print(x)
+# print(y)
+# print(z)
+# x, y, z = 10
+# print(x)
+# print(y)
+# print(z)
+
+
+# x = 2077
+# y = 2077
+# print(id(x))
+# print(id(y))
+
+# print(id(2077))
+#
+# x = 10
+# y = 15.6789076543
+# z = 1j
+#
+# print(type(x))\]
+# print(type(y))
+# print(type(z))
+
+# print(round(y, 2))
+# print(complex(y))
+# print("Hello, what is your name:")
+# x = input()
+# print("Nice to meet you, " + x)
+# print("How old are you?")
+# x = input()
+# print(f"Your age: {x}")
+
+# a = int(input("Enter first number:"))
+# b = int(input("Enter second number:"))
+# print("Result", a + b, sep=" = ")
+
+# print(pow(3, 2))
+
+
+# print(-5//2)
+# print(5//2)
+#
+# print(11/3)
+# print(11//3)
+# print(-11//3)
+
+# print(7/3)
+# print(7//3)
+# print(-7//3)
+import decimal
+import math
+
+#
+# print(math.trunc(7 / 3))
+# print(math.trunc(7 / 3))
+# print(math.trunc(-7 / 3))
+#
+# print(math.floor(7 / 3))
+# print(math.floor(7 / 3))
+# print(math.floor(-7 / 3))
+#
+#
+# print(round(7 / 3))
+# print(round(7 / 3))
+# print(round(-7 / 3))
+#
+
+# print(math.pi)
+# print(math.e)
+# print(math.sqrt(144))
+# print(sum((1, 2)))
+# print(sum((1, 2, 3, 4)))
+# print(min(1, 2, 3, 4, 5, 0, -10))
+# print(max(1, 2, 3, 4, 5, 0, -10))
+
+
+# print(int(7 / 3))
+# print(int(7 / 3))
+# print(int(-7 / 3))
+
+# print(int(oct(64), 8))
+# print(hex(64))
+# print(bin(64))
+
+
+# import random
+#
+# print(random.random())
+# print(random.randint(1, 10))
+# print(random.choice([1, 2, 3, 4, 5, 6, 7, 99, 100]))
+# lst = [1, 2, 3, 4, 5, 6, 7, 99, 100]
+# random.shuffle(lst)
+# print(lst)
+
+
+# print(0.1 + 0.1 + 0.1 - 0.3)
+# print(int(5.551115123125783e-17))
+# from decimal import Decimal
+# print(Decimal('0.1') + Decimal('0.1') + Decimal('0.1') - Decimal('0.3'))
+# print(Decimal(1) / Decimal(7))
+# decimal.getcontext().prec = 4
+# print(Decimal(1) / Decimal(7))
+
+
+
---
+++ Lesson_2/lection.py
@@ -0,0 +1,63 @@
+#####GIT#####
+# LOCAL
+# GITHUB
+##### Python variables#####
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive
+# A variable name cannot be any of the Python keywords.
+# work with memmory + garbage collector
+# assign multivariables(x, y, z = 10, 19, "Hillel"; x = y = z = "LMS")
+##### Data type: Numeric Types ###### int, float, complex
+# Introduction
+# Type Conversion
+
+##### Python Arithmetic Operators #####
+# +	Addition	x + y
+# -	Subtraction	x - y
+# *	Multiplication	x * y
+# /	Division	x / y
+# %	Modulus	x % y
+# **	Exponentiation	x ** y
+# //	Floor division	x // y - округление в меньшую сторону и усечение
+
+
+###### Python Assignment Operators ####
+# =	x = 5	x = 5
+# +=	x += 3	x = x + 3
+# -=	x -= 3	x = x - 3
+# *=	x *= 3	x = x * 3
+# /=	x /= 3	x = x / 3
+# %=	x %= 3	x = x % 3
+# //=	x //= 3	x = x // 3
+# **=	x **= 3	x = x ** 3
+
+
+
+#### Some built_in_functions
+# abc(-1)
+# round()
+# type()
+# int()
+# float()
+# complex()
+# print
+# pow()
+# sum()
+# min()
+# max()
+# math
+# random
+
+
+#Decimal
+# print(0.1 + 0.1 + 0.1 - 0.3)
+# print(int(5.551115123125783e-17))
+# from decimal import Decimal
+# print(Decimal('0.1') + Decimal('0.1') + Decimal('0.1') - Decimal('0.3'))
+# print(Decimal(1) / Decimal(7))
+# decimal.getcontext().prec = 4
+# print(Decimal(1) / Decimal(7))
+
+
